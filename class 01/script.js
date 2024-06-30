c7a9f98f9701b96ff1de152832adea4e10ca8580
hussainshamim16 <hj422748@gmail.com>
2024-06-30 09:21:03 +0500

    add git

---
+++ class 01/script.js
@@ -0,0 +1,18 @@
+
+
+
+// Es 5 
+// 2015 sey pehly ka javascript ES 5 kehlata hey basic jitny bhi paraha vo 
+// es 5 tha
+
+//Es 6
+// 2015 key baad vala js ES 6 kehlata hey
+
+// var is a es 5 feature
+// let const es 6 feature
+
+// function is a es 5 feature
+// Arrow fnc es 6 feature
+
+var firstName = "Hussain"
+console.log(firstName)
