6e822933c6e6011e13dc73777b392efbe75cd0d0
Arzoo Shamim <nabeelqadri021@gmail.com>
2024-07-07 09:06:55 +0500

    class 03 done

--- class 03/script.js
+++ class 03/script.js
@@ -1,56 +1,87 @@
-console.log("advance javascript class 02")
+/// array method
 
-var state = "pakistan";
-var state = "island";
-// this is a problem
+//find findindex filter map foreach
 
-// so i use that  let
-let city = "karachi"
-// let city = "karachi" //error
+find 
+// find one value of an array Example
+let arrFind = [1,2,3,4,5];
+console.log(arrFind)
 
-// const is a constant value
-const houseNumber = "#1657 sector 71/D"
+let findArray = arrFind.find((value , index)=>{
+    // console.log("value",value,"index",index)
+    if(value === 3){
+        console.log("i have value index",index)
+        return true
+    }
+})
+console.log(findArray);
 
-// var is a function scope variable
-// let and const is a block scope variable
+///findindex 
+/* ye bilokol ap find jesa kaam kar kar dey ga but
+ vo value deta tha ye aap ko index numer dy ga */
 
-function mydata(){
-    console.log("my data is not null")
+ let findIndex = ["hussain","shamim","jaweed","uddin"];
+
+let indexFind = findIndex.findIndex((value , index)=>{
+    
+    // console.log("find value",findIndex[index])
+    console.log("find value",value)
+    if(value === "jaweed"){
+        return index
+    }
+})
+console.log(indexFind);
+
+// array or obj with obj
+
+let obj1 = {
+    name:"hussain"
+}
+let obj2 = {
+    name:"hussain"
 }
 
-// arrow fnc
-const myDta = (pera1,pera2)=>{
-    console.log("my data is not undefine")    
-    return pera1 + pera2
-}
-var value = myDta(20,1)
-console.log(value)
+console.log(obj1 === obj2) // answer is false
 
-const sameData = () => 
-    `${ tayer = {
-        name: "hussain",
-        age: 25
-    },
-console.log("tayar", tayer.name )
-}`
-sameData()
-
-// ternary operator = `back tick`
-// spread operator = ... three dots
-
-// copy array in spread operator
-var copyArrayRefrence = [1,2,3,4,5,6,7];
-var doubleCopy = [...copyArrayRefrence];
-console.log(doubleCopy)
+// object kabhibhi appas mey macth nhi karen gey
 
 
-// copy obj in spread operator
-var realObject = {
-    name: "hussain",
-    age:17,
-    class:11
-};
+// map()
+/* maap hammary bohot jaga kaam aata hy ye array key oper 
+alternet chalta hy or hammay aike new array deta hey jo hamm array key 
+opper editing karty hen os ka example;;; */
+let Tableleght = [1,2,3,4,5,6,7,8,9,10]
+var table = Tableleght.map((value,index)=>{
+    return value*7
+    
+})
+console.log("table 7",table)
 
-var copyObject = {...realObject};
-console.log(copyObject)
+// filter()
+// ye values ko filter kar key apki array valuse ko dhoondth hey
+// ayasa find bhi karta hey maggar vo serif aike value ko dhondta hey
 
+let studentName = ["ali","ahmed","tahir","rehman","jaweed","ahmed","hussain","shamim","ahmed"]
+// console.log(studentName)
+let callingStd = studentName.filter((value,index)=>{
+    // console.log("come here", value)
+    if(value === "ahmed"){
+        return value
+    }
+})
+
+console.log(callingStd)
+
+// for each
+/* ye bilkol for loop ke tarha kaam krta
+ hey ba sthor sente allag hey  */
+ let Nmber = 6;
+let Loping = [1,2,3,4,5,6,7,8,9,10]
+let loper = Loping.forEach((value, index)=>{
+    // console.log("hussain", index,value)
+    // if(index === value){
+        console.log(Nmber," x ",index += 1," = ",value*Nmber)
+
+    // }
+})
+ console.log(Loping)
